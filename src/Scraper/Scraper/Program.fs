39169538633b46179330d6b9c2968ec6a397bc55
Filipe Silva <f.silva@coolblue.nl>
2016-07-07 00:39:39 +0200

    First small progress: Getting house listing links from the site

---
+++ src/Scraper/Scraper/Program.fs
@@ -0,0 +1,17 @@
+﻿open FSharp.Data
+
+[<Literal>]
+let url = "http://www.funda.nl/koop/utrecht/+5km/100000-300000/woonhuis/2+kamers/tuin/3-dagen/"
+
+[<EntryPoint>]
+let main argv = 
+
+    let searchPage = HtmlDocument.Load(url)
+
+    let results = searchPage.CssSelect "div.search-result-content"
+
+    let links = results |> Seq.map (fun result -> result.Descendants ["a"] |> Seq.choose (fun link -> link.TryGetAttribute("href")) |> Seq.head)
+
+
+    0 // return an integer exit code
+
